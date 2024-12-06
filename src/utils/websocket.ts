df72f9bfa14050e601f539b83ea0c48c1ab1407c
Astrokke <nathan@musielak.net>
2024-12-06 12:57:05 +0100

    debug

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -1,79 +1,40 @@
 import { MessageEncryption } from "./encryption";
-import { encode as encodeBase64 } from "@stablelib/base64";
 
 export class WebSocketClient {
   private ws: WebSocket | null = null;
-  private messageHandlers: ((data: any) => void)[] = [];
+  private messageHandler: ((data: any) => void) | null = null;
   private keyPair: nacl.BoxKeyPair;
 
   constructor(private url: string) {
     this.keyPair = MessageEncryption.generateKeyPair();
   }
 
-  async connect(walletAddress: string) {
-    this.ws = new WebSocket(this.url);
+  connect(userId: string) {
+    this.ws = new WebSocket(`${this.url}?userId=${userId}`);
 
-    this.ws.onopen = async () => {
-      const authMessage = {
-        type: "authenticate",
-        walletAddress: walletAddress,
-        publicKey: encodeBase64(this.keyPair.publicKey),
-        timestamp: Date.now(),
-      };
-
-      const signature = await this.signMessage(JSON.stringify(authMessage));
-
-      this.ws?.send(
-        JSON.stringify({
-          ...authMessage,
-          signature,
-        })
-      );
-    };
-
-    this.ws.onmessage = async (event) => {
-      try {
-        const data = JSON.parse(event.data);
-        if (data.encrypted) {
-          data.content = MessageEncryption.decrypt(
-            data.content,
-            data.sender,
-            this.keyPair.secretKey
-          );
-        }
-        this.messageHandlers.forEach((handler) => handler(data));
-      } catch (error) {
-        console.error("WebSocket message error:", error);
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (this.messageHandler) {
+        this.messageHandler(data);
       }
     };
   }
 
-  async sendMessage(message: any) {
-    if (this.ws?.readyState === WebSocket.OPEN) {
-      const encryptedContent = MessageEncryption.encrypt(
-        message.content,
-        message.recipient,
-        this.keyPair.secretKey
-      );
+  onMessage(handler: (data: any) => void) {
+    this.messageHandler = handler;
+  }
 
-      const messageToSend = {
-        ...message,
-        content: encryptedContent,
-        encrypted: true,
-        timestamp: Date.now(),
-      };
-
-      const signature = await this.signMessage(JSON.stringify(messageToSend));
-
-      this.ws.send(
-        JSON.stringify({
-          ...messageToSend,
-          signature,
-        })
-      );
-      return true;
+  sendMessage(message: any) {
+    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+      this.ws.send(JSON.stringify(message));
     }
-    return false;
+  }
+
+  disconnect() {
+    if (this.ws) {
+      this.ws.close();
+      this.ws = null;
+    }
   }
 
   private async signMessage(message: string): Promise<string> {
